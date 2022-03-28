2b8353a604ec4ce04b2ddf269096b69152cad8b5
seokjin <mm8678@g.yju.ac.kr>
2022-03-28 10:12:48 +0900

    Initial commit

---
+++ src/test/java/yj/capstone/aerofarm/AerofarmApplicationTests.java
@@ -0,0 +1,13 @@
+package yj.capstone.aerofarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerofarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
