dbbb059346d2f7613327ae6f87148aca65121385
BaeDongHyeon <sasd730@gmail.com>
2022-05-08 22:11:03 +0900

    커밋 테스트

--- src/main/java/yj/capstone/aerofarm/controller/CommunityController.java
+++ src/main/java/yj/capstone/aerofarm/controller/CommunityController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommunityController {
-    // Test Commit
+    
     @GetMapping("/community")
     public String community() {
 
