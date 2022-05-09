94852f8535e6d3211b6aea8794c23aae49efbf34
jeonbeomsu <217wjs@naver.com>
2022-05-09 11:45:20 +0900

    capstone_6 1. 카카오 sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:capstone/LoginPage/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: 'cf0a2321116751cad7b6b470377c39b3');
   runApp(const MyApp());
 }
 
