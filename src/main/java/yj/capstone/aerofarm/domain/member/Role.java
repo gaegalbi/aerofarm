df42e69873b1dea5d10b3487b1c05af9ae93c376
seokjin <mm8678@g.yju.ac.kr>
2022-05-10 10:08:10 +0900

    - Role 수정 (필드 제거)

--- src/main/java/yj/capstone/aerofarm/domain/member/Role.java
+++ src/main/java/yj/capstone/aerofarm/domain/member/Role.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST", "손님"),
-    MEMBER("ROLE_MEMBER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자");
+    GUEST, MEMBER, ADMIN
+//    GUEST("ROLE_GUEST", "손님"),
+//    MEMBER("ROLE_MEMBER", "일반 사용자"),
+//    ADMIN("ROLE_ADMIN", "관리자");
 
-    private final String key;
-    private final String title;
+//    private final String key;
+//    private final String title;
 }
