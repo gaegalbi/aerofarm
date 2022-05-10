a2a3d0091004ccdbadd9bb1fa1eb6dbd675da5b9
seokjin <mm8678@g.yju.ac.kr>
2022-05-11 02:19:37 +0900

    기존 가입된 회원 있을 시 프사, 이름 변경 안되게 수정 (로그인만 가능하도록) 추후 변경사항 필요 시 주석 참조

--- src/main/java/yj/capstone/aerofarm/service/CustomOAuth2UserService.java
+++ src/main/java/yj/capstone/aerofarm/service/CustomOAuth2UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import yj.capstone.aerofarm.config.auth.dto.OAuthAttributes;
 import yj.capstone.aerofarm.config.auth.dto.SessionUser;
 import yj.capstone.aerofarm.controller.dto.UserDetailsImpl;
@@ -16,12 +17,19 @@ import javax.servlet.http.HttpSession;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final MemberService memberService;
     private final HttpSession httpSession;
 
+    /**
+     * Update시 모든 정보를 수정하기 때문에 readOnly로 해두었음
+     * 그리고 회원가입 시 기존 회원이 있으면 기존 회원의 정보를 그대로 받아오게 했음 (로그인만 되게)
+     * 추후 변경사항 생길 시 해당 옵션 제거 및 saveOrUpdate 메소드 주석 해제
+     */
     @Override
+    @Transactional(readOnly = true)
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
@@ -41,17 +49,16 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         return new UserDetailsImpl(member, oAuth2User.getAttributes());
     }
 
+    /**
+     * 회원가입 시 기존 회원이 있으면 기존 회원의 정보를 그대로 받아오게 했음 (로그인만 되게)
+     * 추후 필요 시 update 주석 해제
+     */
     private Member saveOrUpdate(OAuthAttributes attributes) {
-        if (memberService.duplicateEmailCheck(attributes.getEmail())) {
+        if (memberService.duplicateEmailCheck(attributes.getEmail())) { // 자체 회원가입된 계정이 있는지 검사
             Member member = memberService.findByEmail(attributes.getEmail());
-            member.update(attributes.getName(), attributes.getPicture());
+//            member.update(attributes.getName(), attributes.getPicture());
             return member;
         }
         return memberService.saveOAuth2Member(attributes.toEntity());
-
-//        Member member = memberRepository.findByEmail(attributes.getEmail())
-//                .map(e -> e.update(attributes.getName(), attributes.getPicture()))
-//                .orElse(attributes.toEntity());
-//        return memberRepository.save(member);
     }
 }
