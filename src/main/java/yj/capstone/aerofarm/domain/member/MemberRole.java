df03e643e285b2f1dd0a00ac53f65b99a5a799d7
seokjin <mm8678@g.yju.ac.kr>
2022-05-11 01:40:15 +0900

    - Lombok NoArgsConstructor 적용

--- src/main/java/yj/capstone/aerofarm/domain/member/MemberRole.java
+++ src/main/java/yj/capstone/aerofarm/domain/member/MemberRole.java
@@ -1,12 +1,15 @@
 package yj.capstone.aerofarm.domain.member;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import yj.capstone.aerofarm.domain.BaseEntity;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberRole extends BaseEntity {
 
     @Id
@@ -20,9 +23,6 @@ public class MemberRole extends BaseEntity {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
-
-    public MemberRole() {
-    }
 
     public MemberRole(Role role, Member member) {
         this.role = role;
