71bb2071042d482c1908fb52455b52dd47f883bf
BaeDongHyeon <sasd730@gmail.com>
2022-05-19 21:17:31 +0900

    수정사항 적용

--- src/main/java/yj/capstone/aerofarm/controller/CommunityController.java
+++ src/main/java/yj/capstone/aerofarm/controller/CommunityController.java
@@ -5,10 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommunityController {
-    
+
+    // 전체 게시판 글 목록
     @GetMapping("/community")
     public String community() {
 
         return "/community/communityPage";
     }
+
+    @GetMapping("/community/free")
+    public String community_free() {
+        return "/community/free";
+    }
+    // /community/free 자유게시판 글 목록
+    // /community/free/{boardId} 자유게시판 글 상세보기
+    
 }
