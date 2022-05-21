67a39a76e7fda89b1a940ebfd6e8e971f6c0848b
seokjin <mm8678@g.yju.ac.kr>
2022-05-21 16:42:00 +0900

    EnableJpaAuditing Config 분리, EnableAsync 추가, gitignore 수정

--- src/main/java/yj/capstone/aerofarm/AerofarmApplication.java
+++ src/main/java/yj/capstone/aerofarm/AerofarmApplication.java
@@ -3,9 +3,10 @@ package yj.capstone.aerofarm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableAsync
 public class AerofarmApplication {
 
 	public static void main(String[] args) {
