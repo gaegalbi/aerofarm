8348ea9759bb45b05239cc28d351fae3154d1a4f
seokjin <mm8678@g.yju.ac.kr>
2022-05-21 16:42:00 +0900

    EnableJpaAuditing Config 분리, EnableAsync 추가, gitignore 수정

---
+++ src/main/java/yj/capstone/aerofarm/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package yj.capstone.aerofarm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
