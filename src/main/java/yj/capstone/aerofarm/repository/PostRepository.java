c7d031d748630be472ef6117be55a514b754065f
BaeDongHyeon <sasd730@gmail.com>
2022-05-22 22:45:31 +0900

    목록 조회 작업중

--- src/main/java/yj/capstone/aerofarm/repository/PostRepository.java
+++ src/main/java/yj/capstone/aerofarm/repository/PostRepository.java
@@ -3,5 +3,6 @@ package yj.capstone.aerofarm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yj.capstone.aerofarm.domain.board.Post;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface CommunityRepository extends JpaRepository<Post, Long> {
+
 }
