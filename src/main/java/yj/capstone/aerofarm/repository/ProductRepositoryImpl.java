0e486e1d02271be96372e02fbeb8af68517e93d2
seokjin <mm8678@g.yju.ac.kr>
2022-06-13 04:36:07 +0900

    Querydsl5Support 적용

--- src/main/java/yj/capstone/aerofarm/repository/ProductRepositoryImpl.java
+++ src/main/java/yj/capstone/aerofarm/repository/ProductRepositoryImpl.java
@@ -1,29 +1,27 @@
 package yj.capstone.aerofarm.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
+import yj.capstone.aerofarm.domain.product.Product;
 import yj.capstone.aerofarm.domain.product.ProductCategory;
 import yj.capstone.aerofarm.dto.ProductStoreInfoDto;
 import yj.capstone.aerofarm.dto.QProductStoreInfoDto;
-
-import java.util.List;
+import yj.capstone.aerofarm.repository.support.Querydsl5RepositorySupport;
 
 import static yj.capstone.aerofarm.domain.product.QProduct.*;
 import static yj.capstone.aerofarm.domain.product.QProductReview.*;
 
-@RequiredArgsConstructor
-public class ProductRepositoryImpl implements ProductRepositoryCustom {
+public class ProductRepositoryImpl extends Querydsl5RepositorySupport implements ProductRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
+    public ProductRepositoryImpl() {
+        super(Product.class);
+    }
 
     @Override
     public Page<ProductStoreInfoDto> findProductInfo(ProductCategory category, String order, Pageable pageable) {
-        List<ProductStoreInfoDto> content = queryFactory
+        return applyPagination(pageable,
+                query -> query
                 .select(new QProductStoreInfoDto(
                         product.id.as("productId"),
                         product.imageUrl,
@@ -31,20 +29,17 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
                         product.price.money.as("price"),
                         productReview.review.count().as("reviewCnt"),
                         productReview.score.avg().as("scoreAvg")))
-                .from(productReview)
-                .rightJoin(productReview.product, product)
-                .groupBy(product.id)
+                .from(product)
+                .leftJoin(product.productReviews, productReview)
+                .groupBy(product.id, product.imageUrl, product.name, product.price)
                 .where(categoryEq(category))
                 .limit(pageable.getPageSize())
-                .offset(pageable.getOffset())
-                .fetch();
+                .offset(pageable.getOffset()),
 
-        JPAQuery<Long> countQuery = queryFactory
+                query -> query
                 .select(product.count())
                 .from(product)
-                .where(categoryEq(category));
-
-        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+                .where(categoryEq(category)));
     }
 
     private BooleanExpression categoryEq(ProductCategory category) {
