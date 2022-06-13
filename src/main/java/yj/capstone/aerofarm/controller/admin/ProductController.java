14cb678044e4023101ce81b006656c79851749d6
seokjin <mm8678@g.yju.ac.kr>
2022-06-13 19:59:07 +0900

    ProductController admin 패키지로 이동

--- src/main/java/yj/capstone/aerofarm/controller/admin/ProductController.java
+++ src/main/java/yj/capstone/aerofarm/controller/admin/ProductController.java
@@ -1,4 +1,4 @@
-package yj.capstone.aerofarm.controller;
+package yj.capstone.aerofarm.controller.admin;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import yj.capstone.aerofarm.form.SaveProductForm;
 import yj.capstone.aerofarm.domain.product.Product;
@@ -21,14 +22,14 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/product/save")
+    @GetMapping("/admin/product/save")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public String saveProductForm(Model model) {
         model.addAttribute("saveProductForm", new SaveProductForm());
         return "product/saveProductPage";
     }
 
-    @PostMapping("/product/save")
+    @PostMapping("/admin/product/save")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public String saveProduct(@Valid SaveProductForm saveProductForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -36,6 +37,13 @@ public class ProductController {
         }
         Product product = productService.save(saveProductForm);
         log.info("Product created, id = {}", product.getId());
-        return "redirect:/product/save?create=true";
+        return "redirect:/admin/product/save?create=true";
+    }
+
+    @PostMapping("/admin/product/delete/{productId}")
+    public String deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId); // TODO 이미 주문이 있는 상품에 대해 softDelete 할 것!!!
+        log.info("Product created, id = {}", productId);
+        return "redirect:/store";
     }
 }
