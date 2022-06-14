3e60e9f271bbf1ed87d721703a0f680b2957e887
jeonbeomsu <217wjs@naver.com>
2022-06-14 10:35:56 +0900

    capstone_9 1. csrf 제거 및 게시글 작성 시작

--- lib/LoginPage/LoginPageLogin.dart
+++ lib/LoginPage/LoginPageLogin.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:capstone/LoginPage/LoginPageRegister.dart';
 import 'package:capstone/LoginPage/LoginPageResetPassword.dart';
 import 'package:capstone/MainPage/MainPage.dart';
@@ -8,6 +10,10 @@ import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:get/get.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 
 
 var name;
@@ -72,9 +78,104 @@ class _LoginPageLoginRegisterState extends State<LoginPageLoginRegister> {
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 0.059,
             alignment: Alignment.center,
-            child: const Text(
-              "도시농부 아이디로 로그인하기",
-              style: LoginRegisterPageTheme.content,
+            child: TextButton(
+              onPressed: () async {
+                try{
+                  final response = await http
+                      .get(Uri.http('127.0.0.1:8080', '/login'));
+                  //dom.Document document = parser.parse(response.body);
+                  //dom.Element? keywordElements = document.querySelector('.flutter-login');
+
+
+                 //String? token = keywordElements?.outerHtml.substring(63,99);
+                  String? session = response.headers['set-cookie']?.substring(11,43);
+
+                  var map = <String, dynamic>{};
+                  map['email'] = '217wjs@naver.com';
+                  map['password'] = '12';
+                  //map['_csrf'] = token;
+
+                  final response1 = await http.post(
+                    Uri.http('127.0.0.1:8080', '/login'),
+                    headers: {
+                      "Content-Type": "application/x-www-form-urlencoded",
+                      "Cookie":"JSESSIONID=$session",
+                    },
+                    encoding: Encoding.getByName('utf-8'),
+                    body: map,
+                  );
+
+                 /* print(token);
+                  dom.Document document1 = parser.parse(response3.body);
+                  dom.Element? keywordElements1 = document1.querySelector('._csrf');
+                  //print(keywordElements1?.outerHtml);
+                  token = keywordElements1?.outerHtml.substring(42,78);*/
+                  //print(response1.headers);
+
+                  session = response1.headers['set-cookie']?.substring(11,43);
+
+                  //print(response1.headers);
+                  //var headers = response1.headers;
+                  //print(headers);
+                  //print(session);
+                  var map1 = <String, dynamic>{};
+                  map1['category'] = "free";
+                  map1['title'] = "플러터 글쓰기제목";
+                  map1['contents'] = "플러터 글쓰기내용";
+                  var body = json.encode(map1);
+
+                  print(body);
+                  //printWrapped(document2.outerHtml);
+                  final response2 = await http.post(
+                    Uri.http('127.0.0.1:8080', '/community/createPost'),
+                    headers: {
+                    "Content-Type": "application/json",
+                    "Cookie":"JSESSIONID=$session",
+                  },
+                    encoding: Encoding.getByName('utf-8'),
+                    body: body,
+                  );
+                  dom.Document document2 = parser.parse(response2.body);
+                  print(document2.outerHtml);
+
+                  //print(response1.body);
+                  //print(keywordElements);
+                  //printWrapped(response.body);
+                  //print(response.body);
+                  //print(response.headers);
+
+
+                    //final authToken = cookies[1].split(';')[0]; //it depends on how your server sending cookie
+                    //save this authToken in local storage, and pass in further api calls.
+
+                    //aToken = authToken; //saving this to global variable to refresh current api calls to add cookie.
+                    //print(authToken);
+
+
+                  /*final response = await http
+                      .get(Uri.http('172.25.2.57:8080', '/login'));
+                  if (response.statusCode == 200) {
+                    print(response.headers);
+                  }else{
+                    print("notwork");
+                  }*/
+                  /*final response = await http.post(
+                    Uri.http('172.25.2.57:8080', '/community/createPost'),
+                    headers: <String, String>{
+                      'Content-Type': 'application/json; charset=UTF-8',
+                      'Cookie': 'JSESSIONID=123123123'
+                    },
+                  );*/
+
+                }catch(error){
+                  print(error);
+                }
+
+              },
+              child: const Text(
+                "도시농부 아이디로 로그인하기",
+                style: LoginRegisterPageTheme.content,
+              ),
             ),
           ),
           Container(
