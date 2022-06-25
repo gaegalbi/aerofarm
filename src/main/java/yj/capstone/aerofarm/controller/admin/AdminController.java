92e027b6347b80d5b2d381c433920bcfba667b8f
seokjin <mm8678@g.yju.ac.kr>
2022-06-25 16:57:07 +0900

    어드민 컨트롤러 작성, ProductController 어드민 기능 분리, try문 분리

---
+++ src/main/java/yj/capstone/aerofarm/controller/admin/AdminController.java
@@ -0,0 +1,13 @@
+package yj.capstone.aerofarm.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin/adminPage";
+    }
+}
