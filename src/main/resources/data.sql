7f33afb4ae131bd4e6d14d01b5f040192c614bca
seokjin <mm8678@g.yju.ac.kr>
2022-06-27 10:09:12 +0900

    기타 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,18 @@
-insert into member (email, nickname, password, phone_number, picture, provider, score, verify) values ('admin@admin.com', 'admin', '$2a$10$VZk/p9U7oYQQqhS4lrv45.Ih.ZRN.Sll5EO9zQ.xWQz3KSqktVzpe', '010-0000-0000', '/image/default-avatar.png', 'LOCAL', 10, true);
-insert into member_role (member_id, role) values (1, 'GUEST');
-insert into member_role (member_id, role) values (1, 'MEMBER');
-insert into member_role (member_id, role) values (1, 'ADMIN');
+insert into member (email, nickname, password, phone_number, picture, provider, score, verify, created_date)
+values ('admin@admin.com', 'admin', '$2a$10$VZk/p9U7oYQQqhS4lrv45.Ih.ZRN.Sll5EO9zQ.xWQz3KSqktVzpe', '010-0000-0000',
+        '/image/default-avatar.png', 'LOCAL', 10, true, '2022-06-25 21:06:24.599072');
+insert into member_role (member_id, role)
+values (1, 'GUEST');
+insert into member_role (member_id, role)
+values (1, 'MEMBER');
+insert into member_role (member_id, role)
+values (1, 'ADMIN');
 
-insert into member (email, nickname, password, phone_number, picture, provider, score, verify) values ('user@user.com', 'user', '$2a$10$VZk/p9U7oYQQqhS4lrv45.Ih.ZRN.Sll5EO9zQ.xWQz3KSqktVzpe', '010-0000-0000', '/image/default-avatar.png', 'LOCAL', 10, true);
-insert into member_role (member_id, role) values (2, 'GUEST');
+insert into member (email, nickname, password, phone_number, picture, provider, score, verify, created_date)
+values ('user@user.com', 'user', '$2a$10$VZk/p9U7oYQQqhS4lrv45.Ih.ZRN.Sll5EO9zQ.xWQz3KSqktVzpe', '010-0000-0000',
+        '/image/default-avatar.png', 'LOCAL', 10, true, '2022-06-25 21:06:24.599072');
+insert into member_role (member_id, role)
+values (2, 'GUEST');
 
 -- insert into PRODUCT (NAME, PRICE, STOCK, CATEGORY) values ( 'Apple', 1000, 100, 'ETC');
 -- insert into PRODUCT (NAME, PRICE, STOCK, CATEGORY) values ( 'Pear', 1000, 100, 'ETC');
