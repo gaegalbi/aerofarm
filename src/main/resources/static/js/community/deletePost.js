8e8e625b018d02a3bd1fbbfbafa1efff0f640cc4
BaeDongHyeon <sasd730@gmail.com>
2022-06-30 09:53:11 +0900

    댓글의 답글 작성 기능 구현

--- src/main/resources/static/js/community/deletePost.js
+++ src/main/resources/static/js/community/deletePost.js
@@ -1,8 +1,6 @@
 function deletePost(postId) {
-
     let data = {
-        // id: $('#btn-deleteComment').val()
-        id: postId
+        id: Number(postId.substring(12, postId.length))
     };
     $.ajax({
         type: 'POST',
@@ -12,7 +10,7 @@ function deletePost(postId) {
         data: JSON.stringify(data),
     }).done(function () {
         alert('게시글이 삭제 처리 되었습니다.');
-        window.location.href ='/community/' + $('#post-category').val() + '/' + $('#post-id').val();
+        window.location.href ='/community/detail/' + $('#post-id').val();
         // $('#comment-area').load(window.location.href + ' #comment-area');
     }).fail(function (error) {
         alert(JSON.stringify(error));
