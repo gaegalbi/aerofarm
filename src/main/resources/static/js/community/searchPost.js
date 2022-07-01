c3872b63534a4c696760d0472108f62441f515c4
BaeDongHyeon <sasd730@gmail.com>
2022-07-01 11:27:35 +0900

    게시글 검색 초기화 버튼 추가, 필터링과 검색 중첩으로 검색 가능

--- src/main/resources/static/js/community/searchPost.js
+++ src/main/resources/static/js/community/searchPost.js
@@ -1,15 +1,35 @@
-function searchPost() {
-    window.location.href = $('#hidden_category').val() +
-        "?page=1&searchCategory=" + $('#search_category').val() +
-        "&keyword=" + $('#keyword').val();
-}
+function searchPost(searchType, filterValue) {
+    const myUrl = new URL(window.location.href);
+    const urlParam = myUrl.searchParams;
 
-function filtering() {
-    window.location.href = $('#hidden_category').val() +
-        "?page=1&filter=" + $('#hidden-filter').val();
-}
-
-function select_filter(filter) {
-    $('#hidden-filter').val(filter);
-    filtering();
+    let searchUrl = window.location.href.split('?')[0];
+     searchUrl += $('#hidden_category').val() + "?page=1";
+    if (searchType == 'filter') {
+        if (urlParam.has('filter')) {
+            searchUrl += "&filter=" + urlParam.get('filter');
+        } else {
+            searchUrl += "&filter=" + filterValue;
+        }
+        if (urlParam.has('searchCategory')) {
+            searchUrl += "&searchCategory=" + urlParam.get('searchCategory');
+        }
+        if (urlParam.has('keyword')) {
+            searchUrl += "&keyword=" + urlParam.get('keyword');
+        }
+    } else {
+        if (urlParam.has('filter')) {
+            searchUrl += "&filter=" + urlParam.get('filter');
+        }
+        if (urlParam.has('searchCategory')) {
+            searchUrl += "&searchCategory=" + urlParam.get('searchCategory');
+        } else {
+            searchUrl += "&searchCategory=" + $('#search_category').val();
+        }
+        if (urlParam.has('keyword')) {
+            searchUrl += "&keyword=" + urlParam.get('keyword');
+        } else {
+            searchUrl += "&keyword=" + $('#keyword').val();
+        }
+    }
+    window.location.href = searchUrl;
 }
